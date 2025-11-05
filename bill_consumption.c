0ee680f38910fec020d5c0c5da291444af80a303
Kshitij Rastogi <100682522+Kshitij2420@users.noreply.github.com>
2025-11-05 10:08:31 +0530

    Update bill_consumption.c

--- bill_consumption.c
+++ bill_consumption.c
@@ -1,26 +1,34 @@
-#include<stdio.h>
-int main(){
-	int amt;
-	int unit;
-	printf("Enter units \n");
-	scanf("%d",&unit);
-	if(unit>0 && unit<50){
-		amt = 0;
-	}
-	else if(unit>=50 && unit<100){
-		amt = unit*1;
-	}
-	 else if(unit>=100 && unit<200){
-	 	amt = (100*1)+ (unit- 99)*2; 
-	 }
-	 else if(unit>=200 && unit<300){
-	 	amt = 100*1 + 200*2 + (unit-199)*3;
-	 }
-	 else if(unit>=300 && unit<400){
-	 	amt = 100*1 + 200*2 + 300*3 + (unit-299)*4;
-	 }
-	 else{
-	    amt = 100*1 + 200*2 + 300*3 + 400*4 + (unit-399)*5;
-	 }
-printf("bill amount = %d",amt);
+#include <stdio.h>
+
+int main() {
+    int unit;
+    float amt = 0;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &unit);
+
+    if (unit <= 50) {
+        amt = 0;
+    } 
+    else if (unit <= 100) {
+        amt = (unit - 50) * 1;
+    } 
+    else if (unit <= 200) {
+        amt = (50 * 0) + (50 * 1) + (unit - 100) * 2;
+    } 
+    else if (unit <= 300) {
+        amt = (50 * 0) + (50 * 1) + (100 * 2) + (unit - 200) * 3;
+    } 
+    else if (unit <= 400) {
+        amt = (50 * 0) + (50 * 1) + (100 * 2) + (100 * 3) + (unit - 300) * 4;
+    } 
+    else {
+        amt = (50 * 0) + (50 * 1) + (100 * 2) + (100 * 3) + (100 * 4) + (unit - 400) * 5;
+    }
+
+    printf("Total Bill Amount = Rs. %.2f\n", amt);
+
+    return 0;
 }
+
+
