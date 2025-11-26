2aa8f7e00ffb91f173bc941a22c53e2a3a648631
Kshitij Rastogi <100682522+Kshitij2420@users.noreply.github.com>
2025-11-26 09:46:06 +0530

    Add files via upload

---
+++ 2D_array_odd.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int a[3][3],i,j;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("enter a number: ");
+			scanf("%d",&a[i][j]);
+		}
+	}
+//		printf("for loop ends");
+		for(i=0;i<3;i++){
+			for(j=0;j<3;j++){
+				if(a[i][j]%2!=0){
+					a[i][j]=0;
+				}
+			}
+		}
+		
+		for(i=0;i<3;i++){
+			printf("\n");
+		for(j=0;j<3;j++){
+			printf("%d ",a[i][j]);
+			
+		}
+	}
+
+}
---
+++ matrix multiplication.c
@@ -0,0 +1,27 @@
+//Matrix Addition
+#include<stdio.h>
+int main(){
+	int a[3][3],i,j,b[3][3];
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("enter a number: ");
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("enter a number for second matrix ");
+		for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("enter a number: ");
+			scanf("%d",&b[i][j]);
+		}
+	}
+	int c[3][3];
+	
+			for(i=0;i<3;i++){
+			printf("\n");
+		for(j=0;j<3;j++){
+			c[i][j]= a[i][j]*b[i][j];
+			printf("%d ",c[i][j]);
+		}
+	}
+}
